a739a7d1fbce3dd4af2045be512afb5a3a827b25
Dustin Wright <nzeroxpd@gmail.com>
2020-06-16 14:58:53 -0500

    Added starting project from start.spring.io.

---
+++ springbootsolo/src/main/java/dev/dustinwright/springbootsolo/SpringbootsoloApplication.java
@@ -0,0 +1,13 @@
+package dev.dustinwright.springbootsolo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootsoloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootsoloApplication.class, args);
+	}
+
+}
---
+++ springbootsolo/src/test/java/dev/dustinwright/springbootsolo/SpringbootsoloApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dustinwright.springbootsolo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootsoloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
