5f74f368e2d39fcc022fae7ea11356f936c8b270
Dustin Wright <nzeroxpd@gmail.com>
2020-06-16 19:36:40 -0500

    Added package bootstrap, added the class BootStrapData.  This implements CommandLineRunner.  Added the Component annotation.

---
+++ springbootsolo/src/main/java/dev/dustinwright/springbootsolo/bootstap/BootStrapData.java
@@ -0,0 +1,18 @@
+package dev.dustinwright.springbootsolo.bootstap;
+
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by DW 06/16/20
+ */
+
+@Component
+public class BootStrapData implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+        
+    }
+}
+
