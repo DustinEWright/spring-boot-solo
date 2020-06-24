7ac1487e9239ecade511f7721aa07fe030db6633
Dustin Wright <nzeroxpd@gmail.com>
2020-06-24 13:30:12 -0500

    Added whitespace between the id and the other fields in the Automobile class.

--- springbootsolo/src/main/java/dev/dustinwright/springbootsolo/domain/Automobile.java
+++ springbootsolo/src/main/java/dev/dustinwright/springbootsolo/domain/Automobile.java
@@ -12,6 +12,7 @@ public class Automobile {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    
     private String modelName;
     private String stockNumber;
 
